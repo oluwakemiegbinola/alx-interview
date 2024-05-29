72d9f5ad3b35481db17dc273d8188efe52c44b77
oluwakemiegbinola <oluwakemiegbinola@gmail.com>
2024-05-29 09:29:05 +0000

    interview

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,19 +1,59 @@
 #!/usr/bin/python3
+"""_summary_
+"""
 
-""" Rotate 2D Matrix 90 Degrees Clockwise"""
+
+def transpose_matrix(matrix, n):
+    """_summary_
+
+    Args:
+                    matrix (_type_): _description_
+    """
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+def reverse_matrix(matrix):
+    """_summary_
+
+    Args:
+                    matrix (_type_): _description_
+    """
+    for row in matrix:
+        row.reverse()
 
 
 def rotate_2d_matrix(matrix):
-    """ Function for rotating 2D Matrix 90 degrees clockwise
+    """_summary_
 
-    Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+    Args:
+                    matrix (_type_): _description_
+    """
+    n = len(matrix)
+    # print(n)
 
-    - Do not return anything. The matrix must be edited in-place.
-    - You can assume the matrix will have 2 dimensions and will not be empty.
+    """sample matrix
+    1 2 3
+    4 5 6
+    7 8 9
     """
 
-    n = len(matrix[0])
+    # transpose matrix
+    """
+    1 4 7
+    2 5 8
+    3 6 9
+    """
 
-    for i in range(n - 1, -1, -1):
-        for j in range(0, n):
-            matrix[j].append(matrix[i].pop(0))
+    transpose_matrix(matrix, n)
+
+    # reverse matrix
+    """
+    7 4 1
+    8 5 2
+    9 6 3
+    """
+    reverse_matrix(matrix)
+
+    return matrix
